05f4b07fe7514fdc0251f08974ffd0fa14bea146
marmon7515-ISMS <marmon7515@gmail.com>
2026-03-24 15:02:04 +0100

    Update profile-defaults.ts

--- lib/profile-defaults.ts
+++ lib/profile-defaults.ts
@@ -1,14 +1,1 @@
-import { ProfileInput } from "@/types";
-
-export const EMPTY_PROFILE: ProfileInput = {
-  cloudServices: false,
-  softwareDevelopment: false,
-  suppliersCritical: false,
-  remoteWorkforce: false,
-  physicalOfficeControl: false,
-  criticalProcesses: false,
-  personalData: false,
-  regulatedSector: false,
-  customerDescription: "",
-  uploadedContext: ""
-};
+export { EMPTY_PROFILE } from "@/lib/profile-input";
